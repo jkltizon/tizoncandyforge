18ed562b50057153313294474373ef48af8d5250
jkltizon <jkltizon@gmail.com>
2025-12-03 21:07:56 +0800

    Update footer.php

--- PRLM MODULE2/footer.php
+++ PRLM MODULE2/footer.php
@@ -1,10 +1,10 @@
 <?php
-// footer.php
+
 ?>
 </main>
 
 <footer style="background:#080d16;border-top:2px solid #2e4cff;padding:14px 10px;text-align:center;font-size:12px;color:#e5e7eb;">
-    © <?php echo date("Y"); ?> Tizon Candy Forge • Built with brave code & sweet logic 🍬🛡️
+    © <?php echo date("Y"); ?> John Kenjie L. Tizon 2025
 </footer>
 
 </body>
